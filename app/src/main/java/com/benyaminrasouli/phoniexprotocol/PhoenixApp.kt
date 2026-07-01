0c1435101f395b7d200e68dcc91d42e060f64df2
Benyamin Rasouli <benyaminrasouli@users.noreply.github.com>
2026-07-01 18:34:37 +0330

    feat: Hilt WorkManager configuration for widget worker

--- app/src/main/java/com/benyaminrasouli/phoniexprotocol/PhoenixApp.kt
+++ app/src/main/java/com/benyaminrasouli/phoniexprotocol/PhoenixApp.kt
@@ -1,16 +1,26 @@
 package com.benyaminrasouli.phoniexprotocol
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import androidx.work.WorkManager
 import com.benyaminrasouli.phoniexprotocol.di.WorkerModule
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class PhoenixApp : Application() {
+class PhoenixApp : Application(), Configuration.Provider {
 
     @Inject
     lateinit var workManager: WorkManager
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 
     override fun onCreate() {
         super.onCreate()
